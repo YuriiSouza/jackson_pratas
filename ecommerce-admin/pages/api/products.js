a7a532e695c5b60926245eea15cb05a64db70070
YuriiSouza <yuripeixoto1112@gmail.com>
2025-03-19 23:29:26 -0300

    feat: create the POST to create products

--- ecommerce-admin/pages/api/products.js
+++ ecommerce-admin/pages/api/products.js
@@ -1,3 +1,27 @@
-export default function handle(req, res) {
-  res.json(req.method);
+import { prisma } from "@/lib/prisma";
+
+export default async function handle(req, res) {
+  const {method} = req;
+
+  if (method == 'POST') {
+    // try {
+    const{name, description, price} = req.body;
+
+    const product = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price: parseFloat(price)
+      },
+    });
+
+    return res.status(201).json(product);
+    // } catch (error) {
+    //   console.error("Error to create the product", error);
+    //   return res.status(500).json({error: "error to create the product"});
+    // }
+  // }
+
+  // return res.status(405).json({error: "Method no permited"})
+  }
 }
--- ecommerce-admin/prisma/migrations/20250320022803_init/migration.sql
+++ ecommerce-admin/prisma/migrations/20250320022803_init/migration.sql
@@ -76,6 +76,30 @@ CREATE TABLE `Authenticator` (
     PRIMARY KEY (`userId`, `credentialID`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` TEXT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `stock` INTEGER NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Category_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
